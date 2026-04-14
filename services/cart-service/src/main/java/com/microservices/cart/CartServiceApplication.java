5ced88e30fb5fa8b1f38d650488d50c442503db4
Alexsilvaa9 <alejandrosilvarodriguez9@gmail.com>
2026-04-14 19:13:11 +0200

    feat: integracion con kubernetes completada

--- services/cart-service/src/main/java/com/microservices/cart/CartServiceApplication.java
+++ services/cart-service/src/main/java/com/microservices/cart/CartServiceApplication.java
@@ -2,7 +2,6 @@ package com.microservices.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(
@@ -10,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
                 "com.microservices.core",
                 "com.microservices.cart"
         }
-)@EnableDiscoveryClient
+)
 @EnableFeignClients
 
 
